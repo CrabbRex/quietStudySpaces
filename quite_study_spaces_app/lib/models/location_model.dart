6e2a9c1d48f0e56debf0a94fe912f16be2f6116e
Rex <crabbrex@gmail.com>
2025-04-29 14:22:28 +1000

    Add Model and State for Location data.

---
+++ quite_study_spaces_app/lib/models/location_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Location{
+  final String id;
+  final String name;
+  final String description;
+  final String location;
+  final String lat;
+  final String long;
+
+  Location({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.location,
+    required this.lat,
+    required this.long,
+  });
+}
---
+++ quite_study_spaces_app/lib/states/locationState.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:quite_study_spaces_app/models/location_model.dart';
+
+class Locationstate extends ChangeNotifier{
+  final List<Location> _userLocations = [
+    Location(
+      id: "1",
+      name: "Marcelo's Cafe",
+      description: "Nice cafe near Bondi",
+      location: "227 Bondi Road",
+      lat: "-33.89427687142927",
+      long: "151.2665040116401",
+      ),
+  ];
+
+  List<Location> getLocations() {
+    return _userLocations;
+  }
+
+  void addLocation(Location locationToAdd) {
+    _userLocations.add(locationToAdd);
+    notifyListeners();
+  }
+
+  void deleteLocation(Location locationToAdd) {
+    _userLocations.removeWhere((loc) => loc.id == locationToAdd.id);
+    notifyListeners();
+  }
+}
