8bc23f447c80970aa7db431f9623cbb138e8a93b
Rex <crabbrex@gmail.com>
2025-05-01 15:26:03 +1000

    Add login screen textfields and buttons.

--- quite_study_spaces_app/lib/screens/login_screen.dart
+++ quite_study_spaces_app/lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quite_study_spaces_app/screens/login_screen.dart';
+import 'package:quite_study_spaces_app/states/locationState.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,11 +13,41 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Quiet Study Spaces"), actions: [],),
+      appBar: AppBar(
+        title: Text("Quiet Study Spaces"),
+        actions: [],
+      ),
+      body: Consumer<Locationstate>(builder: (context, locationState, child) {
+        return Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "UserName"),
+              ),
+            ),
+            SizedBox(height: 40),
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password"),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: Placeholder.new,
+              child: Text('Log In'),
+            ),
+          ],
+        ));
+      }),
     );
   }
 }
