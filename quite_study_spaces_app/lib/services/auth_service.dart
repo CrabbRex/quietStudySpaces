ada13d53bf2fc726f88d0dbb6a5447c28919f15d
Rex <crabbrex@gmail.com>
2025-05-06 16:46:08 +1000

    Add Auth services functions.

---
+++ quite_study_spaces_app/lib/services/auth_service.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  Future<String?> signUp({
+    //Parameters
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email, 
+        password: password
+      );
+      return 'Sucess';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The Password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        return 'An Account already exists with that email';
+      } else {
+        return e.message.toString();
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String?> logIn({
+    //Parameters
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email, 
+        password: password
+      );
+      return 'Sucess';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return 'No user found for that email';
+      } else if (e.code == 'wrong-password') {
+        return 'Wrong password';
+      } else {
+        return e.message;
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
